3103c3f28f6999258d750128aa478b036eba56d4
Coda Hale <coda.hale@gmail.com>
2020-10-31 16:15:39 -0600

    Add timeseries service.

---
+++ eventflow-timeseries-srv/src/main/java/io/eventflow/timeseries/srv/TimeseriesImpl.java
@@ -0,0 +1,94 @@
+package io.eventflow.timeseries.srv;
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.Statement;
+import com.google.cloud.spanner.TimestampBound;
+import com.google.protobuf.util.Timestamps;
+import io.eventflow.timeseries.api.GetRequest;
+import io.eventflow.timeseries.api.GetResponse;
+import io.eventflow.timeseries.api.TimeseriesGrpc;
+import io.grpc.stub.StreamObserver;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+public class TimeseriesImpl extends TimeseriesGrpc.TimeseriesImplBase {
+
+  public static final String QUERY_FRONT = "SELECT FORMAT_TIMESTAMP(@fmt, timestamp, @tz), ";
+  private static final String QUERY_BACK =
+      "(value) FROM intervals_minutely WHERE timestamp BETWEEN @start AND @end GROUP BY 1 ORDER BY 1";
+  private final DatabaseClient spanner;
+
+  public TimeseriesImpl(DatabaseClient spanner) {
+    this.spanner = spanner;
+  }
+
+  @Override
+  public void get(GetRequest request, StreamObserver<GetResponse> responseObserver) {
+    var statement =
+        Statement.newBuilder(QUERY_FRONT)
+            .append(aggFunc(request.getAggregation()))
+            .append(QUERY_BACK)
+            .bind("fmt")
+            .to(granFmt(request.getGranularity()))
+            .bind("tz")
+            .to(request.getTimeZone())
+            .bind("start")
+            .to(Timestamp.ofTimeMicroseconds(Timestamps.toMicros(request.getStart())))
+            .bind("end")
+            .to(Timestamp.ofTimeMicroseconds(Timestamps.toMicros(request.getEnd())))
+            .build();
+    try (var results =
+        spanner
+            .readOnlyTransaction(TimestampBound.ofMaxStaleness(1, TimeUnit.MINUTES))
+            .executeQuery(statement)) {
+      var resp = GetResponse.newBuilder();
+      while (results.next()) {
+        resp.addTimestamps(
+            DateTimeFormatter.ISO_ZONED_DATE_TIME
+                .parse(results.getString(0), Instant::from)
+                .getEpochSecond());
+        resp.addValues(results.getLong(1));
+      }
+      responseObserver.onNext(resp.build());
+      responseObserver.onCompleted();
+    }
+  }
+
+  private String granFmt(GetRequest.Granularity granularity) {
+    switch (granularity) {
+      case GRAN_UNKNOWN:
+        throw new IllegalArgumentException("missing granularity");
+      case GRAN_MINUTE:
+        return "%E4Y-%m-%dT%H:%M:00%Ez[%Z]";
+      case GRAN_HOUR:
+        return "%E4Y-%m-%dT%H:00:00%Ez[%Z]";
+      case GRAN_DAY:
+        return "%E4Y-%m-%dT00:00:00%Ez[%Z]";
+      case GRAN_MONTH:
+        return "%E4Y-%m-01T00:00:00%Ez[%Z]";
+      case GRAN_YEAR:
+        return "%E4Y-01-01T00:00:00%Ez[%Z]";
+      default:
+        throw new IllegalArgumentException("unrecognized granularity");
+    }
+  }
+
+  private String aggFunc(GetRequest.Aggregation aggregation) {
+    switch (aggregation) {
+      case AGG_UNKNOWN:
+        throw new IllegalArgumentException("missing aggregation function");
+      case AGG_SUM:
+        return "SUM";
+      case AGG_MAX:
+        return "MAX";
+      case AGG_MIN:
+        return "MIN";
+      case AGG_AVG:
+        return "AVG";
+      default:
+        throw new IllegalArgumentException("unrecognized aggregation function");
+    }
+  }
+}
---
+++ eventflow-timeseries-srv/src/main/java/io/eventflow/timeseries/srv/TimeseriesServer.java
@@ -0,0 +1,61 @@
+package io.eventflow.timeseries.srv;
+
+import com.google.cloud.spanner.DatabaseClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.SpannerOptions;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class TimeseriesServer {
+  private final Server server;
+
+  public TimeseriesServer(DatabaseClient spanner, int port) {
+    this.server = ServerBuilder.forPort(port).addService(new TimeseriesImpl(spanner)).build();
+  }
+
+  public void start() throws IOException {
+    server.start();
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                  System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                  try {
+                    TimeseriesServer.this.stop();
+                  } catch (InterruptedException e) {
+                    e.printStackTrace(System.err);
+                  }
+                  System.err.println("*** server shut down");
+                }));
+  }
+
+  public void stop() throws InterruptedException {
+    if (server != null) {
+      server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+    }
+  }
+
+  private void blockUntilShutdown() throws InterruptedException {
+    if (server != null) {
+      server.awaitTermination();
+    }
+  }
+
+  public static void main(String[] args) throws IOException, InterruptedException {
+    // TODO read params from args
+    var project = "";
+    var instanceId = "";
+    var database = "";
+    var port = 8080;
+
+    try (var spanner = SpannerOptions.newBuilder().build().getService()) {
+      var client = spanner.getDatabaseClient(DatabaseId.of(project, instanceId, database));
+      var server = new TimeseriesServer(client, port);
+      server.start();
+      server.blockUntilShutdown();
+    }
+  }
+}
