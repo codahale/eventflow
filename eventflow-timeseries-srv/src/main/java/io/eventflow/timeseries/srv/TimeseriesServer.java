745eb1d7ffd3059712c50eca1b09d1a0eefdcbed
Coda Hale <coda.hale@gmail.com>
2020-11-02 15:47:27 -0700

    Add caching to timeseries-srv.

--- eventflow-timeseries-srv/src/main/java/io/eventflow/timeseries/srv/TimeseriesServer.java
+++ eventflow-timeseries-srv/src/main/java/io/eventflow/timeseries/srv/TimeseriesServer.java
@@ -1,5 +1,6 @@
 package io.eventflow.timeseries.srv;
 
+import com.github.benmanes.caffeine.cache.CaffeineSpec;
 import com.google.cloud.spanner.DatabaseClient;
 import com.google.cloud.spanner.DatabaseId;
 import com.google.cloud.spanner.SpannerOptions;
@@ -7,14 +8,22 @@ import com.google.common.io.Resources;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import java.io.IOException;
+import java.time.Clock;
+import java.time.Duration;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class TimeseriesServer {
   private final Server server;
 
-  public TimeseriesServer(DatabaseClient spanner, int port) {
-    this.server = ServerBuilder.forPort(port).addService(new TimeseriesImpl(spanner)).build();
+  public TimeseriesServer(
+      DatabaseClient spanner, int port, CaffeineSpec caffeineSpec, Duration maxCacheAge) {
+    var impl = new TimeseriesImpl(spanner);
+    this.server =
+        ServerBuilder.forPort(port)
+            .addService(
+                new CachedTimeseriesImpl(impl, caffeineSpec, Clock.systemUTC(), maxCacheAge))
+            .build();
   }
 
   @SuppressWarnings("CatchAndPrintStackTrace")
@@ -57,9 +66,19 @@ public class TimeseriesServer {
       port = Integer.parseInt(args[3]);
     }
 
+    var cacheSpec = "maximumSize=1000";
+    if (args.length > 4) {
+      cacheSpec = args[4];
+    }
+
+    var maxCacheAge = Duration.ofDays(7);
+    if (args.length > 5) {
+      maxCacheAge = Duration.parse(args[5]);
+    }
+
     try (var spanner = SpannerOptions.newBuilder().build().getService()) {
       var client = spanner.getDatabaseClient(DatabaseId.of(project, instance, database));
-      var server = new TimeseriesServer(client, port);
+      var server = new TimeseriesServer(client, port, CaffeineSpec.parse(cacheSpec), maxCacheAge);
       server.start();
       server.blockUntilShutdown();
     }
