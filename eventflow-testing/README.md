d7e08e66fe083454a895902e5c1cfb30aba94af3
Coda Hale <coda.hale@gmail.com>
2020-11-12 17:10:35 -0700

    Switch to Maven.

--- eventflow-testing/README.md
+++
@@ -1,3 +0,0 @@
-# eventflow-testing
-
-This module provides shared testing libraries for internal use.
--- eventflow-timeseries-api/src/main/java/io/eventflow/timeseries/api/TimeSeriesClient.java
+++ eventflow-timeseries-api/src/main/java/io/eventflow/timeseries/api/TimeSeriesClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Coda Hale
+ * Copyright © 2020 Coda Hale (coda.hale@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
